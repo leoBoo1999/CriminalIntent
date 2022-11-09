2bad25b8daff61e3744533e42087e72d0fcc9c38
billie1999 <bleonel324@gmail.com>
2022-11-08 21:24:44 -0600

    Last Modified

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeDetailFragment.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeDetailFragment.kt
@@ -1,17 +1,26 @@
 package com.bignerdranch.android.criminalintent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.setFragmentResultListener
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import com.bignerdranch.android.criminalintent.databinding.FragmentCrimeDetailBinding
+import kotlinx.coroutines.launch
 import java.util.*
 
-
+//private const val TAG = "CrimeDetailFragment"
 class CrimeDetailFragment: Fragment() {
 
-    //private lateinit var binding: FragmentCrimeDetailBinding
+   // private lateinit var crime: Crime
 
     private var _binding: FragmentCrimeDetailBinding? = null
     private val binding
@@ -19,8 +28,10 @@ class CrimeDetailFragment: Fragment() {
             "Cannot access binding because it is null. Is the view visible?"
         }
 
-    private lateinit var crime: Crime
-    override fun onCreate(savedInstanceState: Bundle?) {
+
+    private val args: CrimeDetailFragmentArgs by navArgs()
+
+    /*override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         crime = Crime(
             id = UUID.randomUUID(),
@@ -28,7 +39,12 @@ class CrimeDetailFragment: Fragment() {
             date = Date(),
             isSolved = false
         )
+        Log.d(TAG, "The crime ID is: ${args.crimeId}")
+    }*/
+    private val crimeDetailViewModel: CrimeDetailViewModel by viewModels {
+        CrimeDetailViewModelFactory(args.crimeId)
     }
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -45,15 +61,32 @@ class CrimeDetailFragment: Fragment() {
         super.onViewCreated(view, savedInstanceState)
         binding.apply {
             crimeTitle.doOnTextChanged { text, _, _, _ ->
-                crime = crime.copy(title = text.toString())
+                crimeDetailViewModel.updateCrime { oldCrime ->
+                    oldCrime.copy(title = text.toString())
+                }
+
             }
-            crimeDate.apply {
-                text = crime.date.toString()
-                isEnabled = false
-            }
+
             crimeSolved.setOnCheckedChangeListener { _, isChecked ->
-                crime = crime.copy(isSolved = isChecked)
+                crimeDetailViewModel.updateCrime { oldCrime ->
+                    oldCrime.copy(isSolved = isChecked)
+                }
+                            }
+        }
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                crimeDetailViewModel.crime.collect { crime ->
+                    crime?.let { updateUi(it) }
+                }
             }
+        }
+        setFragmentResultListener(
+            DatePickerFragment.REQUEST_KEY_DATE
+        ) { _, bundle ->
+
+            val newDate =
+                bundle.getSerializable(DatePickerFragment.BUNDLE_KEY_DATE) as Date
+            crimeDetailViewModel.updateCrime { it.copy(date = newDate) }
         }
 
     }
@@ -62,5 +95,19 @@ class CrimeDetailFragment: Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding= null
+    }
+    private fun updateUi(crime: Crime) {
+        binding.apply {
+            if (crimeTitle.text.toString() != crime.title) {
+                crimeTitle.setText(crime.title)
+            }
+            crimeDate.text = crime.date.toString()
+            crimeDate.setOnClickListener {
+                findNavController().navigate(
+                    CrimeDetailFragmentDirections.selectDate(crime.date)
+                )
+            }
+            crimeSolved.isChecked = crime.isSolved
+        }
     }
 }
--- app/src/main/java/database/CrimeDao.kt
+++ app/src/main/java/database/CrimeDao.kt
@@ -2,6 +2,7 @@ package database
 
 import androidx.room.Dao
 import androidx.room.Query
+import androidx.room.Update
 import com.bignerdranch.android.criminalintent.Crime
 import java.util.*
 import kotlinx.coroutines.flow.Flow
@@ -12,4 +13,6 @@ interface CrimeDao {
    fun  getCrimes(): Flow<List<Crime>>
     @Query("SELECT * FROM crime WHERE id=(:id)")
     suspend fun getCrime(id: UUID): Crime
+    @Update
+    suspend fun updateCrime(crime: Crime)
 }
